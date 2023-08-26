42e7b0a80a8ea8e2318f717910791b4f7bc86a18
Lại Nguyễn Vĩnh Phú <59522866+phutaekwondo@users.noreply.github.com>
2023-08-26 18:15:59 +0700

    Make skils type text bold

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -34,11 +34,11 @@ export default function About() {
             </p>
             <p><span style={{color: info.baseColor}}>skills/tools <span
                 className={Style.green}>(main)</span> $</span> ls</p>
-            <p style={{color: info.baseColor}}> Proficient With</p>
+            <p style={{color: info.baseColor}}><strong> Proficient With</strong></p>
             <ul className={Style.skills}>
                 {info.skills.proficientWith.map((proficiency, index) => <li key={index}>{proficiency}</li>)}
             </ul>
-            <p style={{color: info.baseColor}}> Exposed To</p>
+                <p style={{color: info.baseColor}}><strong> Exposed To</strong></p>
             <ul className={Style.skills}>
                 {info.skills.exposedTo.map((skill, index) => <li key={index}>{skill}</li>)}
             </ul>
