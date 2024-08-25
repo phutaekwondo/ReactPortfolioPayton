858067d1a22e3ad20b7222eb9226c2b5cdf7c36b
phutaekwondo <lainguyenvinhphu@gmail.com>
2024-08-25 13:50:52 +0700

    add resume to nav bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,13 @@
-import React, {useState} from 'react';
+import { Box } from "@mui/material";
+import React, { useState } from 'react';
+import { Link, useLocation } from "react-router-dom";
+import resume from "../assets/LAINGUYENVINHPHU_resume.pdf";
+import logo from '../img/logo.png';
+import { info } from "../info/Info";
 import Style from './Navbar.module.scss';
 import Toggler from "./home/Toggler";
-import {Link, useLocation} from "react-router-dom";
-import {Box} from "@mui/material";
-import {info} from "../info/Info";
-import logo from '../img/logo.png';
 
 const links = [
-    {
-        name: 'Home',
-        to: '/',
-        active: 'home'
-    },
     {
         name: 'About Me',
         to: '/about',
@@ -30,32 +26,37 @@ const links = [
     }
 ]
 
-export default function Navbar({darkMode, handleClick}) {
+export default function Navbar({ darkMode, handleClick }) {
     const location = useLocation()
     const [active, setActive] = useState(location.pathname === '/' ? 'home' : location.pathname.slice(1, location.pathname.length));
 
     return (
         <Box component={'nav'} width={'100%'}>
             <Box component={'ul'} display={'flex'} justifyContent={'center'} alignItems={'center'}
-                 gap={{xs: '2rem', md: '8rem'}}
-                 textTransform={'lowercase'} fontSize={'0.7rem'}>
+                gap={{ xs: '2rem', md: '8rem' }}
+                textTransform={'lowercase'} fontSize={'0.7rem'}>
+                <li>
+                    <a href={resume} target="_blank">
+                        resumé
+                    </a>
+                </li>
                 {links.map((link, index) => (
                     <Box key={index} component={'li'} className={(link.active === active && !link.type) && Style.active}
-                         sx={{borderImageSource: info.gradient}}>
+                        sx={{ borderImageSource: info.gradient }}>
                         <Link to={link.to} onClick={() => setActive(link.active)} className={Style.link}>
-                            {!link.type && <p style={{padding: '0.5rem 0'}}>{link.name}</p>}
+                            {!link.type && <p style={{ padding: '0.5rem 0' }}>{link.name}</p>}
                             {link.type && <Box component={'img'} src={logo} alt={'logo'} padding={'0.5rem'}
-                                                sx={{
-                                                    height: 60,
-                                                    width:  60,
-                                                    maxHeight: 70,
-                                                    maxWidth:  70,
-                                                }}/>}
+                                sx={{
+                                    height: 60,
+                                    width: 60,
+                                    maxHeight: 70,
+                                    maxWidth: 70,
+                                }} />}
                         </Link>
                     </Box>
                 ))}
                 <li>
-                    <Toggler darkMode={darkMode} handleClick={handleClick}/>
+                    <Toggler darkMode={darkMode} handleClick={handleClick} />
                 </li>
             </Box>
         </Box>
